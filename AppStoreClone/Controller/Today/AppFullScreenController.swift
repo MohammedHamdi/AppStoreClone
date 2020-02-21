a40656054fc57c668f0067e0d8227f5570b18e93
Mohammed Hamdi <mhah000@hotmail.com>
2020-02-21 14:46:12 +0200

    -Add Floating app widget at the bottom of the fullscreen in todayView.

--- AppStoreClone/Controller/Today/AppFullScreenController.swift
+++ AppStoreClone/Controller/Today/AppFullScreenController.swift
@@ -42,6 +42,8 @@ class AppFullScreenController: UIViewController, UITableViewDelegate, UITableVie
 //        let height = view.window?.windowScene?.statusBarManager?.statusBarFrame.height ?? 44
         let height = UIApplication.shared.statusBarFrame.height
         tableView.contentInset = .init(top: 0, left: 0, bottom: height, right: 0)
+        
+        setupFloatingControls()
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -87,6 +89,48 @@ class AppFullScreenController: UIViewController, UITableViewDelegate, UITableVie
         closeButton.anchor(top: view.safeAreaLayoutGuide.topAnchor, leading: nil, bottom: nil, trailing: view.trailingAnchor, padding: .init(top: 12, left: 0, bottom: 0, right: 0), size: .init(width: 80, height: 40))
         
         closeButton.addTarget(self, action: #selector(handleDismiss), for: .touchUpInside)
+    }
+    
+    fileprivate func setupFloatingControls() {
+        let floatingContainerView = UIView()
+        floatingContainerView.layer.cornerRadius = 16
+        floatingContainerView.clipsToBounds = true
+        view.addSubview(floatingContainerView)
+        
+//        let bottomPadding = view.window?.windowScene?.statusBarManager?.statusBarFrame.height ?? 0
+        let bottomPadding = UIApplication.shared.statusBarFrame.height
+        floatingContainerView.anchor(top: nil, leading: view.leadingAnchor, bottom: view.bottomAnchor, trailing: view.trailingAnchor, padding: .init(top: 0, left: 16, bottom: bottomPadding, right: 16), size: .init(width: 0, height: 90))
+        
+        let blurVisualEffectView = UIVisualEffectView(effect: UIBlurEffect(style: .regular))
+        floatingContainerView.addSubview(blurVisualEffectView)
+        blurVisualEffectView.fillSuperview()
+        
+        // SubViews
+        let imageView = UIImageView(cornerRadius: 16)
+        imageView.image = todayItem?.image
+        imageView.constrainHeight(constant: 68)
+        imageView.constrainWidth(constant: 68)
+        
+        let getButton = UIButton(title: "GET")
+        getButton.setTitleColor(.white, for: .normal)
+        getButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
+        getButton.backgroundColor = .darkGray
+        getButton.layer.cornerRadius = 16
+        getButton.constrainWidth(constant: 80)
+        getButton.constrainHeight(constant: 32)
+        
+        let stackView = UIStackView(arrangedSubviews: [
+            imageView,
+            VerticalStackView(arrangedSubviews: [
+                UILabel(text: todayItem?.category ?? "Title Here", font: .boldSystemFont(ofSize: 16)),
+                UILabel(text: todayItem?.title ?? "Subtitle Here", font: .systemFont(ofSize: 14))
+            ], spacing: 4),
+            getButton
+        ], customSpacing: 18)
+        
+        floatingContainerView.addSubview(stackView)
+        stackView.fillSuperview(padding: .init(top: 0, left: 16, bottom: 0, right: 16))
+        stackView.alignment = .center
     }
     
 //    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
